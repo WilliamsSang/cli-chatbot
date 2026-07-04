5ef97dd8ba1c2019f6280b4570b6ec8957fbaff3
WilliamsSang <boyuansang7@gmail.com>
2026-07-04 20:41:09 +0800

    Day 3: Add system prompt and conversation memory management

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from openai import OpenAI
 
 load_dotenv()
 client = OpenAI()
-history = []
+history = [
+    {
+        "role": "system",
+        "content": "你叫小含 是我的python学习助手 请你用温柔耐心的风格跟我说话 请你每次回答我的问题的时候不直接告诉我答案而是引导我自己理解问题"
+    }
+]
 while True:
     question = input("你：")
     
